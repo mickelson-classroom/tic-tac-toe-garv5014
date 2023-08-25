f7a4208ca76834afaaf2554dfd3af563368eb8f7
garv5014 <garv@GarvsPC>
2023-08-24 18:59:56 -0600

    components for square and board

--- tic-tac-toes/src/App.js
+++ tic-tac-toes/src/App.js
@@ -1,25 +1,10 @@
 import "./App.css";
 import "./styles.css";
+import Board from "./Board.js";
 
 function App() {
   return (
-    <>
-      <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
-      </div>
-      <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
-      </div>
-      <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
-      </div>
-    </>
+    <Board />
   );
 }
 
